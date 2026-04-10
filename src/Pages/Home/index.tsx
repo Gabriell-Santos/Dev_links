1881cf70abed6813acae12befe06f38db19448fa
Gabriell-Santos <ferreirag.santos.dev@gmail.com>
2026-04-10 00:08:42 -0300

    base principal da página home

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,7 +1,17 @@
 export function Home() {
   return (
-    <div>
-      <h2> Olá, seu Home </h2>
+    <div className="flex flex-col w-full py-4 items-center justify-center">
+      <h1 className=" md:text-4xl  text-5xl font-bold text-white mt-20 ">
+        Meus Links
+      </h1>
+      <span className="text-white mb-5  mt-3 text-2xl">seus links salvos</span>
+      <main className="flex flex-col w-11/12 max-w-xl text-center">
+        <section className="bg-green-300 mb-4 w-full py-2 font-bold text-1xl rounded-lg select-none transition-transform  hover:scale-105 cursor-pointer">
+          <p>
+            <a> Canal no Youtube </a>
+          </p>
+        </section>
+      </main>
     </div>
   );
 }
