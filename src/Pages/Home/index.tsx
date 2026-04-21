4dadbb536966705ba739c63a89ab77444479bae5
Gabriell-Santos <ferreirag.santos.dev@gmail.com>
2026-04-21 17:54:27 -0300

    criado a função de logOut na página admin, e ajustando informações na página home

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -4,10 +4,10 @@ import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
 export function Home() {
   return (
     <div className="flex flex-col w-full py-4 items-center justify-center">
-      <h1 className=" md:text-4xl  text-5xl font-bold text-white mt-20 ">
+      <h1 className=" md:text-4xl  text-5xl font-bold text-white mt-20  mb-5 ">
         Meus Links
       </h1>
-      <span className="text-white mb-5  mt-3 text-2xl">seus links salvos</span>
+
       <main className="flex flex-col w-11/12 max-w-xl text-center">
         <section className="bg-green-300 mb-4 w-full py-2 font-bold text-1xl rounded-lg select-none transition-transform  hover:scale-105 cursor-pointer">
           <p>
